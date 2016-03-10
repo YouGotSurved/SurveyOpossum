7390c575908a175efc4693c37cfacc76ad6e53b1
Joseph DePompeis <josephdepompeis@gmail.com>
2016-03-10 17:09:25 -0500

    starting the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+andrew = Author.create!([{name: "andrew", password: "password" }])
+marcia = Author.create!([{name: "marcia", password: "password" }])
+joseph = Author.create!([{name: "joseph", password: "password" }])
+dennis = Author.create!([{name: "dennis", password: "password" }])
+
+
+andrew_survey = Survey.create!([{title: "title here!", author_id: andrew.first.id, description: "this is the description", link:"test link!"}])
+marcia_survey = Survey.create!([{title: "title here!", author_id: marcia.first.id, description: "this is the description", link:"test link!"}])
+joseph_survey = Survey.create!([{title: "title here!", author_id: joseph.first.id, description: "this is the description", link:"test link!"}])
+dennis_survey = Survey.create!([{title: "title here!", author_id: dennis.first.id, description: "this is the description", link:"test link!"}])
+
+andrew_questions = Question.create!({survey_id: andrew_survey.first.id, order_number: 1, type: Integer, text: "Example Text!", required: true})
